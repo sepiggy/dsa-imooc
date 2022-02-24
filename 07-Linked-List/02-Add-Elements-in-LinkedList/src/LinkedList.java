57a5f3494b7ca829a65965bedea1b2a52a2d919b
sepiggy <jms1209@qq.com>
2022-02-24 23:38:32 +0800

    B4 p62 ok

--- 07-Linked-List/02-Add-Elements-in-LinkedList/src/LinkedList.java
+++ 07-Linked-List/02-Add-Elements-in-LinkedList/src/LinkedList.java
@@ -1,9 +1,12 @@
+// p059. 在链表中添加元素
 public class LinkedList<E> {
 
+    // Node作为LinkedList的内部类, 来作为实际存储元素的地方
     private class Node {
         public E e;
         public Node next;
 
+        // 新创建的Node节点存储的元素是e, 指向的下一个节点是next
         public Node(E e, Node next) {
             this.e = e;
             this.next = next;
@@ -24,8 +27,9 @@ public class LinkedList<E> {
     }
 
     private Node head; // 链表头节点
-    private int size;  // 链表实际存储元素个数
+    private int size;  // 链表实际存储的元素个数
 
+    // 链表构造方法, 初始化头节点为空, size=0
     public LinkedList() {
         head = null;
         size = 0;
@@ -53,15 +57,19 @@ public class LinkedList<E> {
 
     // 在链表的index(0-based)位置添加新的元素e
     // 在链表中不是一个常用的操作，练习用：）
+    // 在链表中间添加元素
+    // 其关键是找到要添加的节点的前一个节点
     public void add(int index, E e) {
 
         if (index < 0 || index > size)
             throw new IllegalArgumentException("Add failed. Illegal index.");
 
-        if (index == 0)
+        if (index == 0) // 链表头没有前一个节点, 所以需要特殊处理, 调用addFirst方法
             addFirst(e);
         else {
+            // prev表示待插入节点的前一个节点
             Node prev = head;
+            // 找到要添加的节点(index)的前一个节点(index-1), 需要遍历index-1次
             for (int i = 0; i < index - 1; i++)
                 prev = prev.next;
 
--- 07-Linked-List/03-DummyHead-in-LinkedList/src/LinkedList.java
+++ 07-Linked-List/03-DummyHead-in-LinkedList/src/LinkedList.java
@@ -1,68 +1,73 @@
+// p060. 使用链表的虚拟头节点
+// 技巧: 使用虚拟头节点创建链表可以统一链表的相关操作
+// 编写相关逻辑会更加方便, 这个实现细节对用户是透明的
 public class LinkedList<E> {
 
-    private class Node{
+    private class Node {
         public E e;
         public Node next;
 
-        public Node(E e, Node next){
+        public Node(E e, Node next) {
             this.e = e;
             this.next = next;
         }
 
-        public Node(E e){
+        public Node(E e) {
             this(e, null);
         }
 
-        public Node(){
+        public Node() {
             this(null, null);
         }
 
         @Override
-        public String toString(){
+        public String toString() {
             return e.toString();
         }
     }
 
-    private Node dummyHead;
+    private Node dummyHead; // 虚拟头节点, 其存储的元素是null, dummyHead.next是真正存储元素的第一个节点
     private int size;
 
-    public LinkedList(){
+    public LinkedList() {
+        // 链表初始化的时候就存在一个头节点, 这个头节点不存储任何元素
+        // 一个空链表本身就存在一个唯一的虚拟头节点, 这个节点存储的元素是null, 指向的下一个元素也是null
         dummyHead = new Node();
         size = 0;
     }
 
     // 获取链表中的元素个数
-    public int getSize(){
+    public int getSize() {
         return size;
     }
 
     // 返回链表是否为空
-    public boolean isEmpty(){
+    public boolean isEmpty() {
         return size == 0;
     }
 
     // 在链表的index(0-based)位置添加新的元素e
     // 在链表中不是一个常用的操作，练习用：）
-    public void add(int index, E e){
+    public void add(int index, E e) {
 
-        if(index < 0 || index > size)
+        if (index < 0 || index > size)
             throw new IllegalArgumentException("Add failed. Illegal index.");
 
-        Node prev = dummyHead;
-        for(int i = 0 ; i < index ; i ++)
+        Node prev = dummyHead; // dummyHead对应0位置这个元素前一个位置的节点
+        for (int i = 0; i < index; i++) // 找到索引index前一个位置的节点，需要遍历index次
             prev = prev.next;
 
         prev.next = new Node(e, prev.next);
-        size ++;
+        size++;
     }
 
     // 在链表头添加新的元素e
-    public void addFirst(E e){
+    public void addFirst(E e) {
         add(0, e);
     }
 
     // 在链表末尾添加新的元素e
-    public void addLast(E e){
+    public void addLast(E e) {
         add(size, e);
     }
 }
